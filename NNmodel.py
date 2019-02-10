c1d9d0fe31e47639f94900102607e32c01c62d75
infamia <smalecbartlomiej@gmail.com>
2019-02-10 14:36:56 +0100

    init

--- NNmodel.py
+++ NNmodel.py
@@ -1,12 +1,13 @@
+import keras
 from keras import Sequential
 from keras.layers import Dense
-import keras
 
 
-def create_model(optimizer='rmsprop',init='glorot_uniform'):
+def create_model(optimizer='rmsprop', neuron_in_first=32, neuron_in_second=64, dropout_in_first=0, dropout_in_second=0,
+                 activation_in_first='relu', activation_in_second='relu'):
     nn = Sequential()
-    nn.add(Dense(32, input_dim=14, activation='relu'))
-    nn.add(Dense(64,init=init, activation='relu'))
+    nn.add(Dense(neuron_in_first, input_dim=14, activation=activation_in_first))
+    nn.add(Dense(neuron_in_second, activation=activation_in_second))
 
     nn.add(Dense(1, activation='sigmoid'))
 
